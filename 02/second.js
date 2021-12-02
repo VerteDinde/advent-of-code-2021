33b4664fd6ccfeca09c53c05d812f4b7b06eaf32
VerteDinde <vertedinde@electronjs.org>
2021-12-02 09:21:21 -0800

    chore: day 2 solution (part 2)

--- 02/second.js
+++ 02/second.js
@@ -2,10 +2,17 @@ const { input } = require('./input');
 
 let depth = 0
 let position = 0;
+let aim = 0;
+
 for (let i = 0; i < input.length; i++) {
   const tick = input[i];
-  if (tick.direction === 'depth') depth += tick.value;
-  if (tick.direction === 'forward') position += tick.value;
+  if (tick.direction === 'depth') {
+    aim += tick.value;
+  }
+  if (tick.direction === 'forward') {
+    position += tick.value;
+    depth += aim * tick.value;
+  }
 }
 
 console.log('Solution (depth * position): ', depth * position);
